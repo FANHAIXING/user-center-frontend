d024045ed08702b561c5a328470784738f6e6c35
Jasper <2251747537@qq.com>
2023-07-09 17:46:08 +0800

    更改production的request前缀

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -13,7 +13,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV ==="production" ? 'http://user-backend.code-nav.cn':undefined
+  prefix: process.env.NODE_ENV ==="production" ? 'http://124.220.92.136/':undefined
   // requestType: 'form',
 });
 
